a1fb9e7ed9470f13070bae39ff4baee0f7c0c91c
Malcolm Lockyer <segfault88@gmail.com>
2024-07-16 20:04:22 +1200

    first commit

---
+++ api/main.py
@@ -0,0 +1,17 @@
+import fastapi
+from fastapi.middleware.cors import CORSMiddleware
+
+app = fastapi.FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
