19cca66e8c0da442d4d67878543a2d9ed0fcf156
Malcolm Lockyer <segfault88@gmail.com>
2024-07-16 20:22:08 +1200

    wip

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import axios from 'axios'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+axios.defaults.baseURL = 'http://localhost:8000/'
 
 app.use(createPinia())
 app.use(router)
---
+++ frontend/src/views/PingView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>Ping: {{ message }}</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      message: '...'
+    }
+  },
+  async created() {
+    const response = await axios.get('http://localhost:8000/')
+    this.message = response.data.message
+  }
+}
+</script>
